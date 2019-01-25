e8451d8a94128174fcff9cd94825656980f90afb
Aaron <aarongladstone456@gmail.com>
2019-01-24 21:07:43 -0600

    added firebase link

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAvvvOlr-YOqg3zdrTOeg8Dnmu1Y-A5SBA",
+    authDomain: "whos-calling-me-1548382086597.firebaseapp.com",
+    databaseURL: "https://whos-calling-me-1548382086597.firebaseio.com",
+    projectId: "whos-calling-me-1548382086597",
+    storageBucket: "",
+    messagingSenderId: "903989663896"
+  };
+  firebase.initializeApp(config);
+ 
