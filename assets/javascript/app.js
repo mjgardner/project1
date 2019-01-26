f43695e760dcc2a96ce571be222e8eaae35e69d6
Aaron <aarongladstone456@gmail.com>
2019-01-26 10:38:11 -0600

    pull from master

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var whitePagesKey = '382fe41a61544d38a63b00ceec5711d3';
 var map;
 var marker;
@@ -64,6 +65,60 @@ $(function() {
       },
       error: ajaxError
     });
+=======
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAvvvOlr-YOqg3zdrTOeg8Dnmu1Y-A5SBA",
+    authDomain: "whos-calling-me-1548382086597.firebaseapp.com",
+    databaseURL: "https://whos-calling-me-1548382086597.firebaseio.com",
+    projectId: "whos-calling-me-1548382086597",
+    storageBucket: "",
+    messagingSenderId: "903989663896"
+  };
+  firebase.initializeApp(config);
+ 
+var api_key = {
+  whitepages: "382fe41a61544d38a63b00ceec5711d3",
+};
+
+function lookup(phoneNumber) {
+  var url = {
+    phone_reputation:
+      "https://proapi.whitepages.com/3.0/phone_reputation?api_key=" +
+      api_key.whitepages +
+      "&phone=" +
+      phoneNumber,
+    reverse_phone:
+      "https://proapi.whitepages.com/3.0/phone?api_key=" +
+      api_key.whitepages +
+      "&phone=" +
+      phoneNumber
+  };
+
+  var results = {};
+  $.ajax({
+    url: url.reverse_phone,
+    method: "GET",
+    success: function(response) {
+      results["reverse_phone"] = response;
+      $.ajax({
+        url: url.phone_reputation,
+        method: "GET",
+        success: function(response) {
+          results["phone_reputation"] = response;
+          console.log(results);
+        },
+        error: function(xhr) {
+          console.log("error: " + xhr.status);
+        }
+      });
+    },
+    error: function(xhr) {
+      console.log("error: " + xhr.status);
+    }
+>>>>>>> pull from master
   });
 });
 
