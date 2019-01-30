50b1b03603edc7060d9760c9807ffb8e0e24b58b
Dara Anya <daraanya@Daras-MacBook-Pro.local>
2019-01-29 18:59:31 -0600

    edits to html/css/js. added info buttons with js functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,138 @@
-var api_key = {
-  whitepages: "382fe41a61544d38a63b00ceec5711d3",
-};
+var whitePagesKey = '382fe41a61544d38a63b00ceec5711d3';
+var map;
+var marker;
 
-function lookup(phoneNumber) {
-  var url = {
-    phone_reputation:
-      "https://proapi.whitepages.com/3.0/phone_reputation?api_key=" +
-      api_key.whitepages +
-      "&phone=" +
-      phoneNumber,
-    reverse_phone:
-      "https://proapi.whitepages.com/3.0/phone?api_key=" +
-      api_key.whitepages +
-      "&phone=" +
-      phoneNumber
-  };
+$(function() {
+  $('#submitbutton').click(function(event) {
+    event.preventDefault();
+    var phoneNumber = $('#exampleInputPhoneNumber').val().trim();
 
-  var results = {};
-  $.ajax({
-    url: url.reverse_phone,
-    method: "GET",
-    success: function(response) {
-      results["reverse_phone"] = response;
-      $.ajax({
-        url: url.phone_reputation,
-        method: "GET",
-        success: function(response) {
-          results["phone_reputation"] = response;
-          console.log(results);
-        },
-        error: function(xhr) {
-          console.log("error: " + xhr.status);
-        }
-      });
-    },
-    error: function(xhr) {
-      console.log("error: " + xhr.status);
-    }
+    var url = {
+      phoneReputation:
+        'https://proapi.whitepages.com/3.0/phone_reputation?api_key=' +
+        whitePagesKey +
+        '&phone=' +
+        phoneNumber,
+      reversePhone:
+        'https://proapi.whitepages.com/3.0/phone?api_key=' +
+        whitePagesKey +
+        '&phone=' +
+        phoneNumber
+    };
+
+    var results = {};
+    $.ajax({
+      url: url.reversePhone,
+      method: 'GET',
+      success: function(response) {
+        results['reversePhone'] = response;
+        $.ajax({
+          url: url.phoneReputation,
+          method: 'GET',
+          success: function(response) {
+            results['phoneReputation'] = response;
+            console.log(results);
+            
+             $("#reputation-level-display").empty(); 
+             $("#report-score-display").empty(); 
+             $("#volume-score-display").empty(); 
+
+             var repuationLevel = response.reputation_level;
+             var categoryType = response.reputation_details.category;
+             var reputationScore = response.reputation_details.score; 
+
+              $("#reputation-level-display").html(repuationLevel);
+              console.log(repuationLevel);
+              $("#report-score-display").html(reputationScore);
+              console.log(reputationScore);
+        
+              if (categoryType === null) {
+              $("#category-type-display").html("None")
+              console.log("None");
+               }
+               
+              else {
+                $("#category-type-display").html(categoryType);
+                console.log(categoryType);
+                }
+
+
+            // set address, map and place marker
+            if (results.reversePhone) {
+              var address = results.reversePhone.current_addresses[0];
+              if (address) {
+                $('#address-display').html(
+                  address.street_line_1 + '<br>' +
+                  (address.street_line_2 ? address.street_line_2 + '<br>' : '') +
+                  address.city + ', ' + address.state_code + ' ' + address.postal_code +
+                  (address.zip4 ? '-' + address.zip4 : '')
+                );
+                if (address.lat_long) {
+                  var latLong = address.lat_long;
+
+                  map = new google.maps.Map(document.getElementById('map-appears-here'), {
+                    center: {lat: latLong.latitude, lng: latLong.longitude},
+                    zoom: 8
+                  });
+                  marker = new google.maps.Marker({
+                    position: {lat: latLong.latitude, lng: latLong.longitude},
+                    map: map,
+                  });
+                }
+              }
+            }
+
+          },
+          error: ajaxError
+        });
+      },
+      error: ajaxError
+    });
+  });
+});
+
+function initMap() {
+  var houstonTxLatLng = {lat: 29.762778, lng: -95.383056};
+  map = new google.maps.Map(document.getElementById('map-appears-here'), {
+    center: houstonTxLatLng,
+    zoom: 8
   });
 }
+
+function ajaxError(xhr) {
+  console.log('error: ' + xhr.status);
+}
+
+$(document).ready(function(){
+
+  // create a function to hide and display information when info icon is pressed
+  $("#reputation-level-info-icon").on("click", function(){
+      var x = document.getElementById("reputation-level-info");
+      // console.log(x.style)
+      if (x.style.display === "none" || !x.style.display) { 
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+  })
+  // create a function to hide and display information when info icon is pressed
+  $("#category-type-info-icon").on("click", function(){
+      var x = document.getElementById("category-type-info");
+      // console.log(x.style)
+      if (x.style.display === "none" || !x.style.display) {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      })
+  // create a function to hide and display information when info icon is pressed
+  $("#report-score-info-icon").on("click", function(){
+      var x = document.getElementById("report-score-info");
+      // console.log(x.style)
+      if (x.style.display === "none" || !x.style.display) {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+  })
+})
