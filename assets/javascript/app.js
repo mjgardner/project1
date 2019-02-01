a5adbafe0ab7b6340c6a2f0721ee9c4cf6ae3ab2
Mark Gardner <mjg@phoenixtrap.com>
2019-02-01 17:58:10 -0600

    format documents with Prettier

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var whitePagesKey = '9c1e6a670dcd4f9599686f5057a57543';
+var whitePagesKey = "9c1e6a670dcd4f9599686f5057a57543";
 var map;
 var marker;
 var firebaseConfig = {
@@ -11,58 +11,75 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-              
+
 var database = firebase.database();
 
 $(document).ready(function() {
-  $('#submitbutton').click(function(event) {
+  $("#submitbutton").click(function(event) {
     event.preventDefault();
-    var phoneNumber = $('#exampleInputPhoneNumber').val().trim();
+    var phoneNumber = $("#exampleInputPhoneNumber")
+      .val()
+      .trim();
 
     var url = {
       phoneReputation:
-        'https://proapi.whitepages.com/3.0/phone_reputation?api_key=' +
+        "https://proapi.whitepages.com/3.0/phone_reputation?api_key=" +
         whitePagesKey +
-        '&phone=' +
+        "&phone=" +
         phoneNumber,
-      reversePhone: 
-        'https://proapi.whitepages.com/3.0/phone?api_key=' +
+      reversePhone:
+        "https://proapi.whitepages.com/3.0/phone?api_key=" +
         whitePagesKey +
-        '&phone=' +
+        "&phone=" +
         phoneNumber
     };
 
     var results = {};
     $.ajax({
       url: url.reversePhone,
-      method: 'GET',
+      method: "GET",
       success: function(response) {
-        results['reversePhone'] = response;
+        results["reversePhone"] = response;
         $.ajax({
           url: url.phoneReputation,
-          method: 'GET',
+          method: "GET",
           success: function(response) {
-            results['phoneReputation'] = response;
+            results["phoneReputation"] = response;
             console.log(results);
-           
+
             results.dateAdded = firebase.database.ServerValue.TIMESTAMP;
 
             database.ref().push(results);
-            
+
             database.ref().on("value", function(childSnapshot) {
               console.log(childSnapshot.val());
 
-              Object.keys(childSnapshot.val()).forEach(function(key, i){
+              Object.keys(childSnapshot.val()).forEach(function(key, i) {
                 if (i > 9) return;
-              
-              
-                var phoneNumber = childSnapshot.val()[key].reversePhone.phone_number;
-                var category = childSnapshot.val()[key].phoneReputation.reputation_details && childSnapshot.val()[key].phoneReputation.reputation_details.category;
-                var repLevel = childSnapshot.val()[key].phoneReputation.reputation_level;
-                var repScore = childSnapshot.val()[key].phoneReputation.reputation_details && childSnapshot.val()[key].phoneReputation.reputation_details.score;
-                var dateAdded = dayjs(childSnapshot.val()[key].dateAdded).format('MMM D YYYY');
 
-                console.log(phoneNumber, category, repLevel, repScore, dateAdded);
+                var phoneNumber = childSnapshot.val()[key].reversePhone
+                  .phone_number;
+                var category =
+                  childSnapshot.val()[key].phoneReputation.reputation_details &&
+                  childSnapshot.val()[key].phoneReputation.reputation_details
+                    .category;
+                var repLevel = childSnapshot.val()[key].phoneReputation
+                  .reputation_level;
+                var repScore =
+                  childSnapshot.val()[key].phoneReputation.reputation_details &&
+                  childSnapshot.val()[key].phoneReputation.reputation_details
+                    .score;
+                var dateAdded = dayjs(
+                  childSnapshot.val()[key].dateAdded
+                ).format("MMM D YYYY");
+
+                console.log(
+                  phoneNumber,
+                  category,
+                  repLevel,
+                  repScore,
+                  dateAdded
+                );
 
                 var newRow = $("<tr>").append(
                   $("<td>").text(dateAdded),
@@ -71,68 +88,72 @@ $(document).ready(function() {
                   $("<td>").text(repLevel),
                   $("<td>").text(repScore)
                 );
-                
+
                 $("#savedScores  > tbody").append(newRow);
               });
             });
 
             var reputationLevel = response.reputation_level;
             var categoryType = response.reputation_details.category;
-            var reputationScore = response.reputation_details.score; 
+            var reputationScore = response.reputation_details.score;
 
             $("#reputation-level-display").html(reputationLevel + "/4");
             console.log(reputationLevel);
             $("#report-score-display").html(reputationScore + "/100");
             console.log(reputationScore);
-        
+
             if (categoryType === null) {
-              $("#category-type-display").html("None")
+              $("#category-type-display").html("None");
               console.log("None");
-            }               
-            else {
+            } else {
               $("#category-type-display").html(categoryType);
               console.log(categoryType);
             }
 
-                  function colorDiv() {
-                    if (reputationScore >= 85 && reputationScore <= 100) {
-                      $("#report-score-display").css("background-color", "red")
+            function colorDiv() {
+              if (reputationScore >= 85 && reputationScore <= 100) {
+                $("#report-score-display").css("background-color", "red");
+              } else if (reputationScore >= 70 && reputationScore <= 84) {
+                $("#report-score-display").css("background-color", "orange");
+              } else if (reputationScore >= 50 && reputationScore <= 69) {
+                $("#report-score-display").css("background-color", "yellow");
+              } else {
+                $("#report-score-display").css("background-color", "green");
+              }
+            }
+            colorDiv();
 
-                    }
-                  else if (reputationScore >= 70 && reputationScore <= 84) {
-                    $("#report-score-display").css("background-color", "orange")
-
-                  }
-                  else if (reputationScore >= 50 && reputationScore <= 69){
-                    $("#report-score-display").css("background-color", "yellow")
-
-                  }
-                      else{
-                      $("#report-score-display").css("background-color", "green")
-                    }
-                  }
-                  colorDiv();
-                              
             // set address, map and place marker
             if (results.reversePhone) {
               var address = results.reversePhone.current_addresses[0];
               if (address) {
-                $('#address-display').html(
-                  (address.street_line_1 ? address.street_line_1 + '<br>' : '') +
-                  (address.street_line_2 ? address.street_line_2 + '<br>' : '') +
-                  address.city + ', ' + address.state_code + ' ' + (address.postal_code ? address.postal_code : '') +
-                  (address.zip4 ? '-' + address.zip4 : '')
+                $("#address-display").html(
+                  (address.street_line_1
+                    ? address.street_line_1 + "<br>"
+                    : "") +
+                    (address.street_line_2
+                      ? address.street_line_2 + "<br>"
+                      : "") +
+                    address.city +
+                    ", " +
+                    address.state_code +
+                    " " +
+                    (address.postal_code ? address.postal_code : "") +
+                    (address.zip4 ? "-" + address.zip4 : "")
                 );
                 if (address.lat_long) {
                   var latLong = address.lat_long;
 
-                  map = new google.maps.Map(document.getElementById('map-appears-here'), {
-                    center: {lat: latLong.latitude, lng: latLong.longitude},
-                    zoom: 8
-                  });
+                  map = new google.maps.Map(
+                    document.getElementById("map-appears-here"),
+                    {
+                      center: { lat: latLong.latitude, lng: latLong.longitude },
+                      zoom: 8
+                    }
+                  );
                   marker = new google.maps.Marker({
-                    position: {lat: latLong.latitude, lng: latLong.longitude},
-                    map: map,
+                    position: { lat: latLong.latitude, lng: latLong.longitude },
+                    map: map
                   });
                 }
               }
@@ -147,47 +168,45 @@ $(document).ready(function() {
 });
 
 function initMap() {
-  var houstonTxLatLng = {lat: 29.762778, lng: -95.383056};
-  map = new google.maps.Map(document.getElementById('map-appears-here'), {
+  var houstonTxLatLng = { lat: 29.762778, lng: -95.383056 };
+  map = new google.maps.Map(document.getElementById("map-appears-here"), {
     center: houstonTxLatLng,
     zoom: 8
   });
 }
 
 function ajaxError(xhr) {
-  console.log('error: ' + xhr.status);
+  console.log("error: " + xhr.status);
 }
-$(document).ready(function(){
-
+$(document).ready(function() {
   // create a function to hide and display information when info icon is pressed
-  $("#reputation-level-info-icon").on("click", function(){
-      var x = document.getElementById("reputation-level-info");
-      // console.log(x.style)
-      if (x.style.display === "none" || !x.style.display) { 
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-  })
+  $("#reputation-level-info-icon").on("click", function() {
+    var x = document.getElementById("reputation-level-info");
+    // console.log(x.style)
+    if (x.style.display === "none" || !x.style.display) {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  });
   // create a function to hide and display information when info icon is pressed
-  $("#category-type-info-icon").on("click", function(){
-      var x = document.getElementById("category-type-info");
-      // console.log(x.style)
-      if (x.style.display === "none" || !x.style.display) {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-      })
+  $("#category-type-info-icon").on("click", function() {
+    var x = document.getElementById("category-type-info");
+    // console.log(x.style)
+    if (x.style.display === "none" || !x.style.display) {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  });
   // create a function to hide and display information when info icon is pressed
-  $("#report-score-info-icon").on("click", function(){
-      var x = document.getElementById("report-score-info");
-      // console.log(x.style)
-      if (x.style.display === "none" || !x.style.display) {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-  })
-})
-
+  $("#report-score-info-icon").on("click", function() {
+    var x = document.getElementById("report-score-info");
+    // console.log(x.style)
+    if (x.style.display === "none" || !x.style.display) {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  });
+});
